69f9bd5ca3adeff8df57b40d1a9766b3b6437da6
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-08-09 20:47:51 -0500

    Complete insertOne example

--- crud.js
+++ crud.js
@@ -1,5 +1,15 @@
 const { MongoClient } = require("mongodb");
 
+async function createListing(client, newListing) {
+  const result = await client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .insertOne(newListing);
+  console.log(
+    `New Listing created with the following id: ${result.insertedId}`
+  );
+}
+
 async function main() {
   const uri =
     "mongodb+srv://admin:nPxSHbXraKBy8cc2@cluster0.v2jnc.mongodb.net/default?retryWrites=true&w=majority";
@@ -8,6 +18,12 @@ async function main() {
 
   try {
     await client.connect();
+    await createListing(client, {
+      name: "Lovely loft",
+      summary: "A charming loft in Paris",
+      bedrooms: 1,
+      bathrooms: 1,
+    });
   } finally {
     await client.close();
   }
