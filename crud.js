6fe4482878409be4f74f83bdeb468e2ecd765840
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-09-15 19:59:44 -0500

    Draft function to return multiple results

--- crud.js
+++ crud.js
@@ -38,6 +38,51 @@ async function findOneListingByName(client, nameOfListing) {
   }
 }
 
+async function findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(
+  client,
+  {
+    minimumNumberOfBedrooms = 0,
+    minimumNumberOfBathrooms = 0,
+    maximumNumberOfResults = Number.MAX_SAFE_INTEGER,
+  } = {}
+) {
+  const cursor = client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .find({
+      bedrooms: { $gte: minimumNumberOfBedrooms },
+      bathrooms: { $gte: minimumNumberOfBathrooms },
+    })
+    .sort({ last_review: -1 })
+    .limit(maximumNumberOfResults);
+
+  const results = await cursor.toArray();
+
+  if (results.length > 0) {
+    console.log(
+      `Found listing(s) with at least ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms:`
+    );
+    results.forEach((result, i) => {
+      date = new Date(result.last_review).toDateString();
+
+      console.log();
+      console.log(`${i + 1}. name: ${result.name}`);
+      console.log(` _id: ${result._id}`);
+      console.log(` bedrooms: ${result.bedrooms}`);
+      console.log(` bathrooms: ${result.bathrooms}`);
+      console.log(
+        ` most recent review date: ${new Date(
+          result.last_review
+        ).toDateString()}`
+      );
+    });
+  } else {
+    console.log(
+      `No listings found with at least ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms`
+    );
+  }
+}
+
 async function main() {
   const uri =
     "mongodb+srv://admin:@cluster0.v2jnc.mongodb.net/?retryWrites=true&w=majority";
@@ -77,7 +122,12 @@ async function main() {
     //     last_review: new Date(),
     //   },
     // ]);
-    await findOneListingByName(client, "Infinite Views 3");
+    // await findOneListingByName(client, "Infinite Views 3");
+    await findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client, {
+      minimumNumberOfBedrooms: 4,
+      minimumNumberOfBathrooms: 2,
+      maximumNumberOfResults: 5,
+    });
   } finally {
     await client.close();
   }
