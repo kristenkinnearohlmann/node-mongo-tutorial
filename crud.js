948ee3bcf89f850e882ebc3337a1c650e4fee641
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-09-29 17:03:48 -0500

    Transition findOneListingByName to functions

--- crud.js
+++ crud.js
@@ -1,21 +1,9 @@
 const { MongoClient } = require("mongodb");
-const { createListing, createMultipleListings } = require("./functions");
-
-async function findOneListingByName(client, nameOfListing) {
-  const result = await client
-    .db("sample_airbnb")
-    .collection("listingsAndReviews")
-    .findOne({ name: nameOfListing });
-
-  if (result) {
-    console.log(
-      `Found a listing in the collection with the name '${nameOfListing}':`
-    );
-    console.log(result);
-  } else {
-    console.log(`No listings found with the name '${nameOfListing}'`);
-  }
-}
+const {
+  createListing,
+  createMultipleListings,
+  findOneListingByName,
+} = require("./functions");
 
 async function findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(
   client,
